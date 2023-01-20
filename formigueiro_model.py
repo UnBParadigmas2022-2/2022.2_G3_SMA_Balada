d9b5bf1c40f8bde3aedee095d2861f207841a89f
Lorrany Oliveira <lolo.souza80@yahoo.com>
2023-01-20 11:53:55 -0300

    Adicionando parametros para a formiga

--- formigueiro_model.py
+++ formigueiro_model.py
@@ -11,11 +11,23 @@ class FormigaAgent(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.comida = 1
+        self.t = turtle.Turtle()
+        self.t.penup()
+        self.t.shape("turtle")
+        self.t.color("green")
+        self.current_x = 0
+        self.current_y = 0
 
     def step(self):
+        new_x = self.current_x + random.randint(-1 * RANGE , RANGE)
+        new_y = self.current_y + random.randint(-1 * RANGE , RANGE)
         # The agent's step will go here.
         # For demonstration purposes we will print the agent's unique_id
-        print("Ola eu sou a formiga, meu cpf eh " + str(self.unique_id) + ".")
+        self.t.goto(new_x,new_y)
+        self.current_x = new_x
+        self.current_y = new_y
+        print("formiga: " + str(self.unique_id) + " andou para :" + str(new_x) +"|" + str(new_y))
+
 
 
 class FormigueiroModel(mesa.Model):
